a6fcbaba4fd77632e9302527304673525b47bfc4
Caio Mafra <gamersb200@gmail.com>
2022-08-21 10:36:43 -0300

    02

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ window.sr = ScrollReveal({reset:true});
 
 sr.reveal('.containerprojetos' , { 
     rotate : {x:0 , y:80, z:0},
-    duration:5000})
+    duration:3000})
 
 
 sr.reveal( '.container',{  
     rotate:{x:100,y:0,z:10},
-    duration:1000
+    duration:2000
 } )
 
 if (document.location.search.match(/type=embed/gi)) {
