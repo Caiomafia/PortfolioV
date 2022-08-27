922f038b66719895bb3a798d648ce22acc2ff631
Caio Mafra <gamersb200@gmail.com>
2022-08-27 13:52:37 -0300

    01

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 window.sr = ScrollReveal({reset:true});
 
 
-sr.reveal('.containerprojetos' , { 
-    rotate : {x:0 , y:80, z:0},
-    duration:3500})
+sr.reveal( '.containerprojetos',{  
+    rotate:{x:100,y:180,z:710},
+    duration:2000
+} )
 
-    sr.reveal('.container' , { 
-        rotate : {x:0 , y:100, z:0},
-        duration:2000})
-
+    sr.reveal( '.container',{  
+        rotate:{x:100,y:400,z:710},
+        duration:9000
+    } )
 
 
 sr.reveal( '.containerhabilidade',{  
