ed3d131242998e0f4e834eb416cef66d448222d6
Caio Mafra <gamersb200@gmail.com>
2022-11-10 08:50:53 -0300

    script.js

--- script.js
+++ script.js
@@ -12,11 +12,6 @@ sr.reveal( '.containerprojetos',{
     } )
 
 
-sr.reveal( '.containerhabilidade',{  
-    rotate:{x:100,y:00,z:710},
-    duration:1500
-} )
-
 if (document.location.search.match(/type=embed/gi)) {
     window.parent.postMessage("resize", "*");
   }
